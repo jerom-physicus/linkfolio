c713f9a6b80c63349257381e303527cff875574b
jeremiah <114723077+jerom-physicus@users.noreply.github.com>
2023-08-25 18:51:00 +0530

    Add files via upload

--- create.js
+++ create.js
@@ -1,3 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+  set,
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFbExkWdCe93Vkxk_6kAJ1itCuCQvL-WI",
+  authDomain: "linkfolio-75f9c.firebaseapp.com",
+  databaseURL: "https://linkfolio-75f9c-default-rtdb.firebaseio.com",
+  projectId: "linkfolio-75f9c",
+  storageBucket: "linkfolio-75f9c.appspot.com",
+  messagingSenderId: "777012925295",
+  appId: "1:777012925295:web:fd62335ab549fb4a198e9a",
+  measurementId: "G-6E7ZR0SLB1",
+};
+const app = initializeApp(firebaseConfig);
+var db = getDatabase(app);
+
+document.getElementById("link-int").addEventListener("input", function () {
+  let link_int = document.getElementById("link-int").value;
+  onValue(ref(db, "linkfolio_users"), function (snapshot) {
+    let users_list = Object.keys(snapshot.val());
+
+    if (users_list.includes(link_int)) {
+      document.querySelector(".green-tick ").style.display = "none";
+      document.querySelector(".grab-btn ").style.display = "none";
+      document.querySelector(".taken-alert ").style.display = "block";
+    } else if (link_int == "") {
+      document.querySelector(".green-tick ").style.display = "none";
+      document.querySelector(".grab-btn ").style.display = "none";
+      document.querySelector(".taken-alert ").style.display = "none";
+    } else {
+      document.querySelector(".green-tick ").style.display = "block";
+      document.querySelector(".grab-btn ").style.display = "block";
+      document.querySelector(".taken-alert ").style.display = "none";
+    }
+  });
+});
+document.querySelector(".grab-btn").addEventListener("click", function () {
+  document.querySelector(".step2").style.display = "grid";
+  document.querySelector(".step1").style.display = "none";
+});
 document.getElementById("upload-photo").addEventListener("input", function () {
   let inputPhoto = document.getElementById("upload-photo").files[0];
   let profile = document.querySelector(".place-img");
